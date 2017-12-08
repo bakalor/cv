91a7de9b674b7a976a393e8262556b66ec68ed0f
a.lavrov <a.lavrov@simcord.com>
2017-12-08 17:00:49 +0200

    Exptract pie configuration into s separated file

--- src/scenes/profile/components/general/index.tsx
+++ src/scenes/profile/components/general/index.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import * as theme from "./theme.css";
-import * as d3pie from "d3pie";
+
 import * as cn from "classnames";
+import { createPreconfiguredPie } from "scenes/profile/components/general/pie";
 
 interface StatItem {
   name: string;
@@ -51,68 +52,14 @@ const _additionalStats: StatItem[] = [
   }
 ];
 
-/*
-  Current type definition is not completed
-  (labels.outer.format: "none" option does not exist)
-*/
-const pieOptions: any = {
-  size: {
-    canvasWidth: 400,
-    canvasHeight: 320,
-    pieOuterRadius: 150,
-    pieInnerRadius: 20
-  },
-  labels: {
-    outer: {
-      format: "none"
-    },
-    percentage: {
-      color: "white",
-      fontSize: 14
-    },
-  },
-  effects: {
-    load: {
-      effect: "default", // none / default
-      speed: 1000
-    },
-    pullOutSegmentOnClick: {
-      effect: "bounce", // none / linear / bounce / elastic / back
-      speed: 300,
-      size: 10
-    },
-    highlightSegmentOnMouseover: true,
-    highlightLuminosity: -0.2
-  },
-  data: {
-    sortOrder: "none",
-    content: [
-      { label: "Linear Programmer", value: 35, color: "#00838f" },
-      { label: "Dealer", value: 35, color: "#1976d2" },
-      { label: "Bussines Bullshit", value: 20, color: "#01579b" },
-      { label: "Rockstar", value: 10, color: "#0d47a1" },
-    ]
-  },
-  tooltips: {
-    enabled: true,
-    type: "placeholder",
-    string: "{label}: {percentage}%",
-    styles: {
-      fadeInSpeed: 172,
-      backgroundOpacity: 0.7,
-      fontSize: 14,
-      padding: 12,
-      top: 20
-    }
-  },
-};
+
 export class GeneralInfoCard extends React.PureComponent {
 
   private _chart: HTMLDivElement;
   private _pie: d3pie.ID3PieChart;
 
   componentDidMount() {
-    this._pie = new d3pie(this._chart, pieOptions);
+    this._pie = createPreconfiguredPie(this._chart);
   }
 
   render() {
