dcbb956adddcbb6af744530319fdb06e12ff65b3
a.lavrov <a.lavrov@simcord.com>
2017-12-10 16:38:59 +0200

    Add draft skill page

---
+++ src/scenes/stack/components/skill-card/index.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import * as theme from "./theme.css";
+import * as cn from "classnames";
+
+
+interface PassedProps {
+  name: string;
+  skills: Skill[];
+}
+
+interface Skill {
+  name: string;
+  level: string;
+  iconClass: string;
+}
+
+export class SkillCard extends React.PureComponent<PassedProps> {
+  render() {
+    return <div className={theme.wrapper}>
+      <div className={theme.header}>
+        <div className={theme.title}>
+          {this.props.name}
+        </div>
+      </div>
+      <div className={theme.body}>
+        {this.props.skills.map(skill => <div className={theme.row}>
+          <div className={cn(theme.icon, skill.iconClass)}></div>
+          <div className={theme.name}>{skill.name}</div>
+          <div className={theme.level}>{skill.level}</div>
+        </div>)}
+      </div>
+    </div>;
+  }
+}
--- src/scenes/stack/index.tsx
+++ src/scenes/stack/index.tsx
@@ -1,9 +1,71 @@
 import * as React from "react";
+import * as theme from "./theme.css";
+import { SkillCard } from "scenes/stack/components/skill-card";
 
-export class Stack extends React.PureComponent{
+const hotFrontend = [
+  {
+    name: "JavaScript",
+    level: "Expert",
+    iconClass: theme.js
+  },
+  {
+    name: "CSS3",
+    level: "High",
+    iconClass: theme.css
+  },
+  {
+    name: "HTML5",
+    level: "Intermediate",
+    iconClass: theme.html5
+  },
+  {
+    name: "TypeScript",
+    level: "High",
+    iconClass: theme.ts
+  },
+  {
+    name: "React.js",
+    level: "High",
+    iconClass: theme.react
+  },
+  {
+    name: "Redux.js",
+    level: "High",
+    iconClass: theme.redux
+  },
+  {
+    name: "Webpack",
+    level: "High",
+    iconClass: theme.webpack
+  },
+];
+const coldFrontend = [
+  {
+    name: "Ember.js",
+    level: "High",
+    iconClass: theme.ember
+  },
+  {
+    name: "Angular.js (1.x.x)",
+    level: "Low",
+    iconClass: theme.angular
+  },
+]
+export class Stack extends React.PureComponent {
   render() {
-    return <div >
-    TODO
+    return <div className={theme.wrapper}>
+      <div className={theme.grid}>
+        <div className={theme.col}>
+          <SkillCard
+            name="Hot stack"
+            skills={hotFrontend} />
+        </div>
+        <div className={theme.col}>
+          <SkillCard
+            name="Cold stack"
+            skills={coldFrontend} />
+        </div>
+      </div>
     </div>;
   }
 }
